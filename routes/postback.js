f437a3c38e718e81c59de73ad43f0b9bebc44adc
Nick Pate <npate-ctr@iwsinc.com>
2018-03-15 13:44:11 -0700

    Added in remote ip to log.

--- routes/postback.js
+++ routes/postback.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET users listing. */
 router.post('/', function(req, res, next) {
-  req.db.insert({result: req.body});
+  req.db.insert({
+    result: req.body,
+    ip: req.headers['x-forwarded-for'] || req.connection.remoteAddress,
+  });
   req.db.ensureIndex({ fieldName: 'createdAt', expireAfterSeconds: 3600 }, function (err) { console.log(err); });
   res.sendStatus(200);
 });
